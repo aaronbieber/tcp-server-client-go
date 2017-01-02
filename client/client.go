495d4f6ebb7849cb9d8f63ac361870af3d97ac2e
Aaron Bieber <aaron@aaronbieber.com>
2017-01-02 15:31:49 -0500

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,96 @@
+/*
+A very simple TCP client written in Go.
+
+This is a toy project that I used to learn the fundamentals of writing
+Go code and doing some really basic network stuff.
+
+Maybe it will be fun for you to read. It's not meant to be
+particularly idiomatic, or well-written for that matter.
+*/
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+var host = flag.String("host", "localhost", "The hostname or IP to connect to; defaults to \"localhost\".")
+var port = flag.Int("port", 8000, "The port to connect to; defaults to 8000.")
+
+func main() {
+	flag.Parse()
+
+	dest := *host + ":" + strconv.Itoa(*port)
+	fmt.Printf("Connecting to %s...\n", dest)
+
+	conn, err := net.Dial("tcp", dest)
+
+	if err != nil {
+		if _, t := err.(*net.OpError); t {
+			fmt.Println("Some problem connecting.")
+		} else {
+			fmt.Println("Unknown error: " + err.Error())
+		}
+		os.Exit(1)
+	}
+
+	go readConnection(conn)
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+
+		conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
+		_, err := conn.Write([]byte(text))
+		if err != nil {
+			fmt.Println("Error writing to stream.")
+			break
+		}
+	}
+}
+
+func readConnection(conn net.Conn) {
+	for {
+		scanner := bufio.NewScanner(conn)
+
+		for {
+			ok := scanner.Scan()
+			text := scanner.Text()
+
+			command := handleCommands(text)
+			if !command {
+				fmt.Printf("\b\b** %s\n> ", text)
+			}
+
+			if !ok {
+				fmt.Println("Reached EOF on server connection.")
+				break
+			}
+		}
+	}
+}
+
+func handleCommands(text string) bool {
+	r, err := regexp.Compile("^%.*%$")
+	if err == nil {
+		if r.MatchString(text) {
+
+			switch {
+			case text == "%quit%":
+				fmt.Println("\b\bServer is leaving. Hanging up.")
+				os.Exit(0)
+			}
+
+			return true
+		}
+	}
+
+	return false
+}
---
+++ server/server.go
@@ -0,0 +1,84 @@
+/*
+A very simple TCP server written in Go.
+
+This is a toy project that I used to learn the fundamentals of writing
+Go code and doing some really basic network stuff.
+
+Maybe it will be fun for you to read. It's not meant to be
+particularly idiomatic, or well-written for that matter.
+*/
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+var addr = flag.String("addr", "", "The address to listen to; default is \"\" (all interfaces).")
+var port = flag.Int("port", 8000, "The port to listen on; default is 8000.")
+
+func main() {
+	fmt.Println("Starting server...")
+
+	src := *addr + ":" + strconv.Itoa(*port)
+	listener, _ := net.Listen("tcp", src)
+	fmt.Printf("Listening on %s.\n", src)
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Some connection error: %s\n", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
+
+	scanner := bufio.NewScanner(conn)
+
+	for {
+		ok := scanner.Scan()
+
+		handleMessage(scanner.Text(), conn)
+
+		if !ok {
+			break
+		}
+	}
+
+	fmt.Println("Client at " + remoteAddr + " disconnected.")
+}
+
+func handleMessage(message string, conn net.Conn) {
+	fmt.Println("> " + message)
+
+	if message[0] == '/' {
+		switch {
+		case message == "/time":
+			resp := "It is " + time.Now().String() + "\n"
+			fmt.Print("< " + resp)
+			conn.Write([]byte(resp))
+
+		case message == "/quit":
+			fmt.Println("Quitting.")
+			conn.Write([]byte("I'm shutting down now.\n"))
+			fmt.Println("< " + "%quit%")
+			conn.Write([]byte("%quit%\n"))
+			os.Exit(0)
+
+		default:
+			conn.Write([]byte("Unrecognized command."))
+		}
+	}
+}
